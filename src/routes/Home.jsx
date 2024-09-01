626d430594a9f450b9a414ff294c810d9650f96f
Nicolas <haubrichtnicolas@gmail.com>
2024-09-01 17:28:56 -0300

    Seção Sobre criada, estilizada e responsiva

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SectionHome from '../components/SectionHome';
+import SectionAbout from '../components/SectionAbout';
 
 const Home = () => {
   return (
     <>
       {/* Section Home */}
       <SectionHome />
+      <SectionAbout />
     </>
   )
 }
