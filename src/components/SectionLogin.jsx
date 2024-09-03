78bd3cac0d9f3ce74bb4e35781cd60dc26dd84eb
Eduardo <eduardoocmadid@gmail.com>
2024-09-03 13:36:04 -0300

    Traducao da pagina de login

--- src/components/SectionLogin.jsx
+++ src/components/SectionLogin.jsx
@@ -1,9 +1,17 @@
 import React, {useRef} from 'react'
+import { useTranslation } from 'react-i18next'
 import { LoginStyle } from '../styles/LoginStyle';
 import LogoMahindra from '../assets/header/logo/mahindra-logo-new.svg'
 import LogoGoogle from '../assets/login/logo-google.svg'
 
 const SectionLogin = () => {
+    // Select Language
+    const { t, i18n } = useTranslation();
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        setShowLanguages(false);
+    };
+
     return (
         <>
         <LoginStyle>
@@ -17,24 +25,24 @@ const SectionLogin = () => {
                         {/* Top */}
                         <div className='top-acc'>
                             <img src={LogoMahindra} alt="" />
-                            <h2>Welcome Back!</h2>
+                            <h2>{t('welcome-back')}</h2>
                         </div>
 
                         {/* Mid */}
                         <div className='mid-acc'>
-                            <form action="/login" method="POST">
-                                <input type="text" name="username" placeholder="Username" required/>
-                                <input type="password" name="password" placeholder="Password" required/>
-                                <a href="/" class="forgot-password">Forgot your password?</a>
+                            <form action="/login">
+                                <input type="text" name="username" placeholder={t('username')} required/>
+                                <input type="password" name="password" placeholder={t('password')} required/>
+                                <a href="/" class="forgot-password">{t('forgot-password')}</a>
                                 <button type="submit">Login</button>
                             </form>
-                            <p>Don't have an account? <button><a href="/sign-up">Sign-Up</a></button></p>
+                            <p>{t('account')} <button><a href="/sign-up">{t('signup')}</a></button></p>
                         </div>
 
                         {/* End */}
                         <div className='end-acc'>
                             <div>
-                                <button><img src={LogoGoogle} alt="#" /> Login with Google</button>
+                                <button><img src={LogoGoogle} alt="#" />{t('login-google')}</button>
                             </div>
                         </div>
                         
