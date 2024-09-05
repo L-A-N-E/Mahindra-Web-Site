e2c89cdc6d19390df78abb70bd2abcfd429748d2
Alice <alicesb2006@gmail.com>
2024-09-05 16:38:38 -0300

    Criando o estilo do Swiper

--- src/components/SectionRaces.jsx
+++ src/components/SectionRaces.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { SectionRacesStyle } from '../styles/SectionRacesStyle'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// import 'swiper/css'
-// import 'swiper/css/effect-coverflow'
-// import 'swiper/css/pagination'
-// import 'swiper/css/navigation'
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
@@ -27,12 +27,12 @@ const SectionRaces = () => {
                     <h1>Circuitos</h1>
                 </div>
 
-                <Swiper id='container-races'
+                <Swiper
                     effect={'coverflow'}
                     grabCursor={true}
                     centeredSlides={true}
-                    loop={true}
-                    slidesPerView={'auto'}
+                    loop={false}
+                    slidesPerView={"auto"}
                     coverflowEffect={
                         {
                             rotate: 0,
@@ -50,12 +50,18 @@ const SectionRaces = () => {
                     modules={[EffectCoverflow, Pagination, Navigation]}
                 >
                     {races.map((race) => (
-                        <SwiperSlide className='card-race' key={race.id}>
-                            <img src={race.country} alt={`Bandeira de ${race.city}`} width="100" />
-                            <h2>Circuito: {race.name}</h2>
-                            <p>Data: {race.data}</p>
-                            <h3>{race.city}</h3>
-                            <img src={race.race_track} alt={`Circuito ${race.name}`} width="100" />
+                        <SwiperSlide key={race.id}>
+                            <div className="container-info">
+                                <div className="img-flag">
+                                    <img src={race.country} alt={`${race.city} Flag`} />
+                                </div>
+                                <h3>{race.city}</h3>
+                                <h2>{race.name}</h2>
+                                <p>{race.data}</p>
+                            </div>
+                            <div className="container-img">
+                                <img src={race.race_track} alt={`Circuit ${race.name}`} className='img-circuit' />
+                            </div>
                         </SwiperSlide>
                     ))}
                 </Swiper>
