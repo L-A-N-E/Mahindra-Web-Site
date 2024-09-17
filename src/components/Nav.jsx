1b282b251180e7ad6b782bb492f13468e273cca9
Nicolas <haubrichtnicolas@gmail.com>
2024-09-16 21:07:29 -0300

    Refazendo Nav Mobile e Desktop

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
-import { NavStyle, NavLogo, NavDesktop, NavItemsDesktop, NavDesktopLanguages, NavMenu, NavMobile, NavItemsMobile, NavMobileLanguages} from '../styles/NavStyle'
+import { NavStyle, NavLogo, NavDesktop, NavItemsDesktop, NavItemsEffect, NavDesktopLanguages, NavMenu, NavMobile, NavItemsMobile, NavMobileLanguages} from '../styles/NavStyle'
 import Arrow from '../assets/footer/arrow.svg'
 import Logo from '../assets/header/logo/mahindra-logo-new.svg'
 
@@ -25,37 +25,43 @@ const Nav = () => {
             <NavDesktop>
                 <NavItemsDesktop>
                     <ul>
+                        {/* Home */}
                         <Link className='txt-none'>
-                            <li>Home</li>
+                            <NavItemsEffect>
+                                <img src={Arrow} alt="Arrow" />
+                                <li>Home</li>
+                            </NavItemsEffect>
                         </Link>
                 
+                        {/* Corridas */}
                         <Link className='txt-none'>
-                            <li>Races</li>
+                            <NavItemsEffect>
+                                <img src={Arrow} alt="Arrow" />
+                                <li>Races</li>
+                            </NavItemsEffect>
                         </Link>
 
+                        {/* Mobile App */}
                         <Link className='txt-none'>
-                            <li>App Mobile</li>
-                        </Link>
-
-
-                        <Link className='txt-none'>
-                            <li>Sing-Up</li>
+                            <NavItemsEffect>
+                                <img src={Arrow} alt="Arrow" />
+                                <li>Mobile App</li>
+                            </NavItemsEffect>
                         </Link>
                         
-                        <Link className='txt-none'>
-                            <li>Login</li>
-                        </Link>
-                        
+                        {/* Idioma */}
                         <NavDesktopLanguages>
-                            <ul className='language' onMouseEnter={()=> setShowOptionsLanguage(true)}>
+
+                            <NavItemsEffect className='language' onMouseEnter={()=> setShowOptionsLanguage(true)}>
                                 <img src={Arrow} alt="Arrow" />
                                 <li>Language</li>
-                            </ul>
+                            </NavItemsEffect>
+
                         {showOptionsLanguage &&(
                             <ul className='language-options' onMouseLeave={()=> setShowOptionsLanguage(false)}>
-                                <li onClick={()=> setShowOptionsLanguage(false)}><Link>English</Link></li>
-                                <li onClick={()=> setShowOptionsLanguage(false)}><Link>Portuguese</Link></li>
-                                <li onClick={()=> setShowOptionsLanguage(false)}><Link>Spanish</Link></li>
+                                <li onClick={()=> setShowOptionsLanguage(false)}>English</li>
+                                <li onClick={()=> setShowOptionsLanguage(false)}>Portuguese</li>
+                                <li onClick={()=> setShowOptionsLanguage(false)}>Spanish</li>
                             </ul>
                         )}
                         </NavDesktopLanguages>
--- src/styles/NavStyle.jsx
+++ src/styles/NavStyle.jsx
@@ -13,6 +13,11 @@ export const NavStyle = styled.header`
     height: 70px;
     z-index: 1000;
     background-color: ${Colors.black};
+    padding: 1rem 3rem;
+
+    @media (max-width: 900px){
+        justify-content: space-between;
+    }
 `;
 
 export const NavLogo = styled.div `
@@ -23,6 +28,8 @@ export const NavLogo = styled.div `
 
 export const NavDesktop = styled.div `
     display: flex;
+    justify-content: center;
+    width: 50%;
     @media (max-width: 900px){
         display: none;
     }
@@ -30,35 +37,87 @@ export const NavDesktop = styled.div `
 
 export const NavItemsDesktop = styled.div `
     display: flex;
+    width: 100%;
     height: 100%;
     align-items: center;
 
     ul {
+        width: 100%;
         display: flex;
         align-items: center;
         justify-content: space-around;
-        gap: 3rem;
 
-        li {
-            list-style: none;
+        ul {
+            display: flex;
+            gap: 0.3rem;
         }
     }
 `
 
+
+export const NavItemsEffect = styled.ul `
+    li {
+        position: relative; 
+        display: inline-block;
+        list-style: none;
+        font-size: ${Font.txt_12};
+        text-transform: uppercase;
+        color: ${Colors.white};
+    }
+
+    li::after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: -3px;
+        width: 0;
+        height: 1px;
+        background-color: ${Colors.red_1};
+        transition: width 0.3s ease-in-out;
+    }
+
+    li:hover::after {
+        width: 100%;
+    }
+
+    img {
+        position: relative;
+        opacity: 0;
+        left: -10px;
+        transition: all 0.3s ease-in-out; 
+    }
+
+    &:hover img {
+        left: 0; /* Movimenta a imagem para a posição original */
+        opacity: 1; /* Torna a imagem visível */
+    }
+`
+
+
+
+
 export const NavDesktopLanguages = styled.div `
-    position: absolute;
-    right: 0;
+
     display: flex;
     flex-direction: column;
 
     .language {
         display: flex;
-
+        gap: 0.3rem;
     }
 
     .language-options {
         display: flex;
         flex-direction: column;
+
+        li {
+            position: relative; 
+            display: inline-block;
+            list-style: none;
+            font-size: ${Font.txt_12};
+            text-transform: uppercase;
+            color: ${Colors.white};
+        }
     }
 `
 
@@ -78,26 +137,18 @@ export const NavMenu = styled.div `
     @media (max-width: 900px) {
         display: flex;
     }
-
-    ${({ showMenu }) =>
-    showMenu &&
-    `
-        span:nth-child(1) {
-            transform: rotate(45deg) translate(5px, 5px);
-        }
-
-        span:nth-child(2) {
-            opacity: 0;
-        }
-
-        span:nth-child(3) {
-            transform: rotate(-45deg) translate(5px, -5px);
-        }
-    `}
 `
 
 export const NavMobile = styled.div `
     display: none;
+    flex-direction: column;
+    position: fixed;
+    right: 0;
+    top: 0;
+    margin-top: 70px;
+    height: 100dvh;
+    width: 50%;
+    background-color: ${Colors.black};
     
     @media (max-width: 900px){
         display: flex;
