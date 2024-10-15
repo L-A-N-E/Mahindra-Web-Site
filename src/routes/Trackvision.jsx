8e3eb424aabc92dd2d5e20040a096c0438aa7728
Nicolas <haubrichtnicolas@gmail.com>
2024-10-15 15:01:18 -0300

    Estilizando TrackVision

--- src/routes/Trackvision.jsx
+++ src/routes/Trackvision.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { TrackvisionStyle, LeftDivStyle, RightDivStyle, InfoSolution, ContainerInfoSolution } from '../styles/TrackvisionStyle'
+import trackvision from '../assets/home/banners/trackvision.png'
+
+const ItemSolution = () => {
+    const trackvisionItems = [
+        {
+            number: '1',
+            title: 'TrackVision',
+            paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quis cum dolores dolorum, corporis sed quia est alias explicabo sit tempore molestias at quasi porro totam dolorem error fugit incidunt.'
+        },
+        {
+            number: '2',
+            title: 'Entenda como funciona',
+            paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quis cum dolores dolorum, corporis sed quia est alias explicabo sit tempore molestias at quasi porro totam dolorem error fugit incidunt.'
+        },
+        {
+            number: '3',
+            title: 'Tenha acesso',
+            paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quis cum dolores dolorum, corporis sed quia est alias explicabo sit tempore molestias at quasi porro totam dolorem error fugit incidunt.'
+        }
+    ];
+
+    return (
+        <InfoSolution>
+            {trackvisionItems.map((item, index) => (
+                <ContainerInfoSolution key={index} className='grid'>
+                    <p className='Number'>{item.number}</p>
+                    <h1 className='TextH1'>{item.title}</h1>
+                    <p className='ParagraphP'>{item.paragraph}</p>
+                </ContainerInfoSolution>
+            ))}
+        </InfoSolution>
+    );
+}
+
 
 const Trackvision = () => {
     return (
-        <div>Trackvision</div>
+        <TrackvisionStyle>
+            <LeftDivStyle>
+                <img src={trackvision} alt="TrackVision" />
+            </LeftDivStyle>
+
+            <RightDivStyle>
+                <ItemSolution />
+            </RightDivStyle>
+        </TrackvisionStyle>
     )
 }
 
---
+++ src/styles/TrackvisionStyle.jsx
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+import Colors from '../constant/Colors';
+import Font from '../constant/Font'
+
+
+export const TrackvisionStyle = styled.section`
+    height: auto;
+    background-color: ${Colors.black};
+    display: flex;
+
+    @media (max-width: 1000px) {
+        flex-direction: column;
+    }
+`
+
+export const LeftDivStyle = styled.div`
+    width: 50%;
+    min-height: (100dvh);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: 1000px) {
+        width: 100%;
+    }
+`
+
+export const RightDivStyle = styled.div`
+    width: 50%;
+    min-height: (100dvh);
+    background-color: ${Colors.black};
+    display: flex;
+    justify-content: center;
+
+    @media (max-width: 1000px) {
+        width: 100%;
+    }
+`
+
+export const InfoSolution = styled.div`
+    display: flex;
+    width: 70%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+
+    @media (max-width: 1000px) {
+        width: 100%;
+    }
+    
+`
+
+export const ContainerInfoSolution = styled.div`
+    display: grid;
+    grid-gap: 1rem; 
+    grid-template-areas: 
+        "Number TextH1"
+        "Number ParagraphP"; 
+    padding: 1rem;
+
+        .Number { 
+            color: ${Colors.white};
+            grid-area: Number; 
+        }
+        .TextH1 { 
+            grid-area: TextH1;
+            height: auto;
+            text-transform: uppercase;
+            color: ${Colors.red_1};
+            font-weight: ${Font.txt_500w};
+        }
+        .ParagraphP { 
+            grid-area: ParagraphP;
+            color: ${Colors.white};
+            text-align: justify;
+        }
+`
