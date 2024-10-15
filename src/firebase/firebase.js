6e38d8e53905d10c41ca2955654b9519ae0f6676
Alice <alicesb2006@gmail.com>
2024-10-15 11:32:01 -0300

    Criando a configuração do storage

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 // Configuração do Firebase
 const firebaseConfig = {
@@ -20,4 +21,6 @@ const app = initializeApp(firebaseConfig);
 // Exporta os serviços
 const auth = getAuth(app);
 const db = getFirestore(app);
-export { app, auth, db };
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
