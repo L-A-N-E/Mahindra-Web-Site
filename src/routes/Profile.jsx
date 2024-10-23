6eae28eb497d764554abc09c5a226a5bc494c341
Nicolas <haubrichtnicolas@gmail.com>
2024-10-23 12:47:46 -0300

    Criando temas e corrigindo uma rota da api na seção pilots do home

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -3,33 +3,48 @@ import React, { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc, setDoc, updateDoc, deleteField } from 'firebase/firestore';
 import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-
 import { useTranslation } from 'react-i18next';
-
 // Importando config firebase
 import { db, storage } from '../firebase/firebase';
-
+import logoFormulaE from '../assets/profile/FE_logo.png'
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 
-import { ProfileStyle, ProfileContainer, ProfileSelectImage, UserContainer, UserProfile, UserInfo, UserText } from '../styles/ProfileStyle'
+import { ProfileStyle, ProfileTheme, ProfileContainer, ProfileContent, ProfileSelectImage, UploadRemoveImage, InsertImage, UserContainer, UserProfile, UserInfo, UserText, Label, StyledSelect } from '../styles/ProfileStyle'
 
 const MySwal = withReactContent(Swal);
 
 const Profile = () => {
 
     const { t, i18n } = useTranslation();
-    
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-    };
-
-    
     const [avatar, setAvatar] = useState(null);
     const [userData, setUserData] = useState({});
     const [error, setError] = useState(null);
     const auth = getAuth();
 
+    const [bgColor, setBgColor] = useState('#000'); // Cor inicial do fundo
+    // Mapeando as cores
+    const options = [
+        { value: '#ED3124', label: 'Andretti Formula E' },
+        { value: '#CBA65F', label: 'DS Penske' },
+        { value: '#00BE26', label: 'Envision Racing' },
+        { value: '#000000', label: 'Jaguar TCS Racing' },
+        { value: '#3c3c3c', label: 'Kiro Race Co' },
+        { value: '#194997', label: 'Lola Yamaha ABT Formula E Team' },
+        { value: '#E51635', label: 'Mahindra Racing' },
+        { value: '#001489', label: 'Maserati MSG Racing' },
+        { value: '#FF8000', label: 'NEOM McLaren Formula E Team' },
+        { value: '#C3002F', label: 'Nissan Formula E Team' },
+        { value: '#D5001C', label: 'TAG Heuer Porsche Formula E Team' },
+    ];
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
+    const handleChange = (e) => {
+        setBgColor(e.target.value); // Define a cor de fundo com base na opção selecionada
+    };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
@@ -61,10 +76,10 @@ const Profile = () => {
                 setAvatar(null);
                 MySwal.fire({
                     title: t('upload-avatar'),
-                    icon:'success',
-                    color:'#fff',
-                    background:'#171717',
-                    confirmButtonColor:'#E51635'
+                    icon: 'success',
+                    color: '#fff',
+                    background: '#171717',
+                    confirmButtonColor: '#E51635'
                 }).then((result) => {
                     if (result.isConfirmed) {
                         window.location.href = '/profile'
@@ -94,10 +109,10 @@ const Profile = () => {
                 setAvatar(null);
                 MySwal.fire({
                     title: t('remove-avatar'),
-                    icon:'success',
-                    color:'#fff',
-                    background:'#171717',
-                    confirmButtonColor:'#E51635'
+                    icon: 'success',
+                    color: '#fff',
+                    background: '#171717',
+                    confirmButtonColor: '#E51635'
                 }).then((result) => {
                     if (result.isConfirmed) {
                         window.location.href = '/profile'
@@ -109,51 +124,76 @@ const Profile = () => {
             setError("Failed to remove avatar.");
             MySwal.fire({
                 title: t('error-avatar'),
-                icon:'error',
-                color:'#fff',
-                background:'#171717',
-                confirmButtonColor:'#E51635'
+                icon: 'error',
+                color: '#fff',
+                background: '#171717',
+                confirmButtonColor: '#E51635'
             }).then((result) => {
                 if (result.isConfirmed) {
                     window.location.href = '/profile'
-                }}) ;
+                }
+            });
         }
     };
 
     return (
         <ProfileStyle>
-            <ProfileContainer>
-                <ProfileSelectImage>
-                    <input
-                        type="file"
-                        onChange={(e) => setAvatar(e.target.files[0])}
-                    />
-                    <button onClick={handleUpload}>Upload</button>
-                    <button onClick={handleRemove}>Remove</button>
-                </ProfileSelectImage>
-                {error && <p>{error}</p>}
+            {/* Tema */}
+            <ProfileTheme bgColor={bgColor}>
+                <img src={logoFormulaE} alt="" />
+            </ProfileTheme>
 
-                <UserContainer>
-                    <UserProfile>
-                        <h3>User Profile</h3>
-                        <p>This is some information about the user.</p>
-                    </UserProfile>
-                    
-                    <UserInfo>
-                        <UserText>
-                            <dt>Username: </dt>
-                            <dd>{userData.username}</dd>
-                        </UserText>
-                        <UserText>
-                            <dt>Email address: </dt>
-                            <dd>{userData.email}</dd>
-                        </UserText>
-                        <UserText>
-                            <dt>Formula E Favorite Team: </dt>
-                            <dd>{userData.favoriteTeam}</dd>
-                        </UserText>
-                    </UserInfo>
-                </UserContainer>
+            {/* Conteudo */}
+            <ProfileContainer bgColor={bgColor}>
+                <ProfileContent>
+                    <ProfileSelectImage>
+                        <InsertImage>
+                            <label>
+                                <span>Select File</span>
+                                <input
+                                    type="file"
+                                    onChange={(e) => setAvatar(e.target.files[0])}
+                                />
+                            </label>
+                        </InsertImage>
+                        <UploadRemoveImage>
+                            <button onClick={handleUpload}>Upload</button>
+                            <button onClick={handleRemove}>Remove</button>
+                        </UploadRemoveImage>
+                    </ProfileSelectImage>
+                    {error && <p>{error}</p>}
+                    <UserContainer>
+                        <UserProfile>
+                            <h3>User Profile</h3>
+                            <p>This is some information about the user.</p>
+                        </UserProfile>
+                        <UserInfo>
+                            <UserText>
+                                <dt>Username: </dt>
+                                <dd>{userData.username}</dd>
+                            </UserText>
+                            <UserText>
+                                <dt>Email address: </dt>
+                                <dd>{userData.email}</dd>
+                            </UserText>
+                            <UserText>
+                                <dt>Formula E Favorite Team: </dt>
+                                <dd>{userData.favoriteTeam}</dd>
+                            </UserText>
+                            <UserText>
+                                <Label>Select a background color:</Label>
+                                <StyledSelect onChange={handleChange}>
+                                    <option value="#000">Choose a color...</option>
+                                    {options.map((option) => (
+                                    <option key={option.value} value={option.value}>
+                                        {option.label}
+                                    </option>
+                                    ))}
+                                </StyledSelect>
+                            </UserText>
+                        </UserInfo>
+                    </UserContainer>
+                </ProfileContent>
             </ProfileContainer>
         </ProfileStyle>
     );
--- src/styles/ProfileStyle.jsx
+++ src/styles/ProfileStyle.jsx
@@ -1,51 +1,98 @@
 import styled from 'styled-components';
 import Colors from '../constant/Colors'
-import backgroundprofile from '../assets/profile/bg-profile.png'
+import Font from '../constant/Font'
 
 export const ProfileStyle = styled.section`
     min-height: 100dvh;
-    display: flex;
-    background-color: ${Colors.dark_gray};
-    background-position: center;
-    background-size: cover;
-    align-items:center;
-    justify-content: center;
+    display: grid; 
+    grid-template-columns: 1fr 1fr; 
+    grid-template-rows: 1fr; 
+    gap: 0px 0px; 
+    grid-template-areas: 
+        "ProfileTheme ProfileContainer";
+
+    @media (max-width:  768px) {
+        display: flex;
+        flex-direction: column;
+    }
+`
+
+export const ProfileTheme = styled.div`
+    grid-area: ProfileTheme;
+    background-color: ${(props) => props.bgColor};
+    transition: background-color 0.3s ease-in-out;
+    height: 100%;
+    display: grid;
+    align-items: center;
+
+    @media (max-width:  768px) {
+        height: 400px;
+    }
 `
 
 export const ProfileContainer = styled.div`
-    width: 90%;
-    min-height: 500px;
-    background-color: ${Colors.gray};
-    color:${Colors.white};
-    padding: 4rem;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-`
-
-export const ProfileSelectImage = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-`;
-
-export const UserContainer = styled.div`
+    grid-area: ProfileContainer;
+    background-color: ${(props) => props.bgColor};
+    transition: background-color 0.3s ease-in-out;
     width: 100%;
     height: 100%;
     display: flex;
-    flex-direction: column;
+    flex-grow: 1;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1rem;
+`  
 
+export const ProfileContent = styled.div`
+    background-color: ${Colors.white};
+    padding: 1rem 2rem;
+    border-radius: 10px;
+`
+
+export const ProfileSelectImage = styled.div`
+
+`;
+export const InsertImage = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    label {
+        span {
+            cursor: pointer;
+        }
+        input {
+            display: none;
+        }
+    }
+`
+
+export const UploadRemoveImage = styled.div `
+
+`
+
+export const UserContainer = styled.div`
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
 `;
 
 export const UserProfile = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+
+    h3 {
+        text-transform: uppercase;
+        font-weight: ${Font.txt_600w};
+    }
 
 `;
 
 export const UserInfo = styled.div`
     width: 100%;
     display: flex;
+    gap: 2rem;
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -53,6 +100,39 @@ export const UserInfo = styled.div`
 
 export const UserText = styled.div`
     display: flex;
-    width: 50%;
+    flex-wrap: wrap;
+    gap: 0.4rem;
+    width: 100%;
     justify-content: space-between;
+
+    dt {
+        text-transform: uppercase;
+        font-size: ${Font.txt_14};
+        font-weight: ${Font.txt_500w};
+    }   
 `;
+
+//  Seleção do Tema
+export const Label = styled.label`
+    font-size: 16px;
+    color: black;
+`
+
+export const StyledSelect = styled.select`
+    padding: 10px 20px;
+    font-size: 16px;
+    border: 2px solid #ccc;
+    border-radius: 5px;
+    background-color: white;
+    cursor: pointer;
+    outline: none;
+
+    &:hover {
+        border-color: #888;
+    }
+
+    @media (max-width: 768px) {
+        padding: 5px 10px;
+        font-size: 14px;
+    }
+`
